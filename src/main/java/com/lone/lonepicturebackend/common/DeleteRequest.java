383e1711b44cc3ba770881eeff6938af9fc1ab53
lonelyli0719 <794156416@qq.com>
2025-05-20 21:38:30 +0800

    云图库第一期后端初始化

---
+++ src/main/java/com/lone/lonepicturebackend/common/DeleteRequest.java
@@ -0,0 +1,21 @@
+package com.lone.lonepicturebackend.common;
+
+import lombok.Data;
+
+import java.io.Serializable;
+
+/**
+ * 请求删除包装类
+ */
+@Data
+public class DeleteRequest implements Serializable {
+
+    /**
+     * id
+     */
+    private Long id;
+
+    private static final long serialVersionUID = 1L;
+}
+
+
---
+++ src/test/java/com/lone/lonepicturebackend/LonePictureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lone.lonepicturebackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LonePictureBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
