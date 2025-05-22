41041c4bb520c478092ff0f381a3e111115a54df
lonelyli0719 <794156416@qq.com>
2025-05-22 23:18:46 +0800

    云图库用户模块

---
+++ src/main/java/com/lone/lonepicturebackend/mapper/UserMapper.java
@@ -0,0 +1,18 @@
+package com.lone.lonepicturebackend.mapper;
+
+import com.lone.lonepicturebackend.model.entity.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Lenovo
+* @description 针对表【user(用户)】的数据库操作Mapper
+* @createDate 2025-05-22 22:28:55
+* @Entity com.lone.lonepicturebackend.model.entity.User
+*/
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+
+
